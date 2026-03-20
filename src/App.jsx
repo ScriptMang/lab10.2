958ca1d1eac13cf55ffb05d3630792157de6f2d5
scriptMang <aperalta686@gmail.com>
2026-03-20 15:10:18 -0400

    Add page-items component wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import PageItems from './components/PageItems'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-
+  const [items, setItems] = useState([]);
   return (
     <>
-  
+    <PageItems items={items}/> 
     </>
   )
 }
---
+++ src/components/PageItems.jsx
@@ -0,0 +1,21 @@
+function PageItems({items, handleList}) {
+    
+    const itemList = items.map(prevItem => {
+            <li key={id}> prevItem</li>
+    })
+    return(
+        <>
+        <h3>Items per page: 
+            <select name="numItems" id="number-of-items-select">
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
+            </select>
+        </h3>
+        {itemList}
+        </>
+    )
+}
+
+export default PageItems
