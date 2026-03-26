b34ec7586163a2d1d72c0e2fedd6a688348a1f11
scriptMang <aperalta686@gmail.com>
2026-03-26 01:07:02 -0400

    Reorganize item select and text that display the total number of items to look better

--- src/components/PageItems.jsx
+++ src/components/PageItems.jsx
@@ -10,15 +10,20 @@ function PageItems({items, totalItems}) {
     return(
         <>
         <h2 id="pageTitle">Pagination Demo</h2>
-        <h3>Items per page: 
-            <select name="numItems" id="number-of-items-select" defaultValue="5" onChange={()=>{}}>
+        <div id="topItemBar">
+            <div id="itemsSelectContainer">
+                <h3>Items per page:</h3> 
+             <select name="numItems" id="number-of-items-select" defaultValue="5" onChange={()=>{}}>
                 <option value="5">5</option>
                 <option value="10">10</option>
                 <option value="15">15</option>
                 <option value="20">20</option>
-            </select>
-        </h3>
-        <div><i>Total Items: {totalItems}</i></div>
+             </select>
+            </div>
+           
+            <div><i>Total Items: {totalItems}</i></div>
+        </div>
+        
         <ul id="currentItemList">{itemList}</ul>
         </>
     )
